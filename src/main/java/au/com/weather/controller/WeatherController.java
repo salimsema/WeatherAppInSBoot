a832bbdc399625664e929dc361f7e681e84ac5a9
salimsema <51860065+salimsema@users.noreply.github.com>
2019-06-26 12:38:39 +1000

    some sys outs added for testing

--- src/main/java/au/com/weather/controller/WeatherController.java
+++ src/main/java/au/com/weather/controller/WeatherController.java
@@ -36,6 +36,7 @@ public class WeatherController {
 	 */
 	@RequestMapping(value=DEFAULTACTION, method = RequestMethod.GET)
 	public String landingPage(ModelMap model) {
+		System.out.println("Inside landig page method");
 		OpenWeatherMap owm;
 		ArrayList<City> cityList = helperClass.populateCityDropDown();
 		model.addAttribute(CITYLIST, cityList); 
@@ -55,6 +56,7 @@ public class WeatherController {
 	 */
 	@PostMapping(value=ACTIONHOME)
 	public String postMethod(ModelMap model,@RequestParam(value=PARAMVALUECITYDROPDOWN, required=false) Integer cityCode) {
+		System.out.println("Inside post method");
 		ArrayList<City> cityList = helperClass.populateCityDropDown();
 	    model.addAttribute(CITYLIST, cityList);
 		OpenWeatherMap owm;
